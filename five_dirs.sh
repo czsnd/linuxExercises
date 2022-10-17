172778c425846652a2c205814d7a2c51841269de
YizheGe <yge43@wisc.edu>
2022-10-17 17:32:04 -0500

    five_dir result

--- five_dirs.sh
+++ five_dirs.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+mkdir five
+for i in 1 2 3 4 5
+do
+
+ mkdir five/dir${i}
+ 
+ for j in 1 2 3 4
+ do
+ 
+  for k in $(seq 1 ${j})
+  do 
+   echo ${j} >> five/dir${i}/file${j}
+  done
+  
+ done
+ 
+done
+
