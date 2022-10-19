b71df75ad3fac5ee047085c314c40874d5ced08f
YizheGe <yge43@wisc.edu>
2022-10-19 09:08:08 -0500

    finish the rm_n.sh

--- rm_n.sh
+++ rm_n.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+dir=$1
+file_size=$2
+if [[ $# -ne 2 ]]; then
+    echo "usage: $0 <dir> <n>" 1>&2
+    exit 0
+fi
+function rm_n {
+
+    echo $(find ./$dir -type 'f' -size -${file_size}c -delete)
+    
+}
+rm_n $dir $file_size
+
